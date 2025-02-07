19b594754ea6c5239771beb914f993e498586261
Frank Gerlach <frankgerlach.tai@gmx.de>
2025-02-07 14:16:20 +0100

    Modernisierung SFwidget und Kompilierung unter Cygwin

--- output/callbacks.cpp
+++ output/callbacks.cpp
@@ -1,4 +1,18 @@
+#include "ProjektIncludes.h"
+
+
+
+
+#include <iostream>
+#include <queue>
+#include <atomic>
+
 #include "header.h"
+
+
+
+using namespace std;
+
 
 
 void callback1(Fl_Widget* widget,void* data)
@@ -12,3 +26,33 @@ void buttonCallback(Fl_Widget* widget,void* data)
    char sp;
    sfb->callback(&sp); 
 }
+
+void checkbox_callback(Fl_Widget *w, void *data) 
+{
+    Fl_Check_Button *check = (Fl_Check_Button*)w;
+    
+    //cout << "checkbox_callback() " << w << endl;
+    SFbutton* sfb = (SFbutton*)data; 
+    char sp;
+    sfb->callback(&sp);
+}
+
+void choice_callback(Fl_Widget *w, void *data) 
+{
+    Fl_Choice* flChoice = (Fl_Choice*) w;
+    
+    SFChoice* sfc = (SFChoice*)data; 
+    char sp;
+    sfc->callback(&sp, flChoice->value() );
+}
+
+void scrollbar_callback(Fl_Widget* w, void* data)
+{
+    Fl_Scrollbar* flScrollbar = (Fl_Scrollbar*)w;
+
+    SFScrollbar* ssb = (SFScrollbar*)data;
+    char sp;
+    ssb->callback(&sp, flScrollbar->value());
+}
+
+
